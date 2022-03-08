2553b0ad9571889ec63a31f9818dc0d7fd82e9de
dhaneshvg <dhanesh.v.gddd@gmail.com>
2022-03-08 13:07:11 +0530

    created generic class view and fix all problems

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path('', views.task_view, name='task_view'),
-    path('delete/<int:taskid>/', views.delete, name='delete'),
-    path('edit/<int:updateid>/', views.update, name='update')
+    path('', views.TaskListView.as_view(), name='cbvtask'),
+    path('cbvdetail/<int:pk>/', views.TaskDetailView.as_view(), name='cbvdetail'),
+    path('cbvupdate/<int:pk>/', views.TaskUpdateView.as_view(), name='cbvupdate'),
+    path('cbvdelete/<int:pk>/', views.TaskDeleteView.as_view(), name='cbvdelete'),
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,33 +1,47 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, UpdateView, DeleteView
+
 from .models import Task
-from .forms import Todoforms
 
 
 # Create your views here.
 
-def task_view(request):
-    display = Task.objects.all()
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        priority = request.POST.get('priority')
-        date = request.POST.get('date')
-        obj = Task(name=name, priority=priority, date=date)
-        obj.save()
-    return render(request, 'task_view.html', {'tasks': display})
+class TaskListView(ListView):
+    model = Task
+    template_name = 'task_view.html'
+
+    def get(self, request, *args, **kwargs):
+        display = Task.objects.all()
+        return render(request, self.template_name, {'tasks': display})
+
+    def post(self, request, *args, **kwargs):
+        if request.method == 'POST':
+            name = request.POST.get('name')
+            priority = request.POST.get('priority')
+            date = request.POST.get('date')
+            obj = Task(name=name, priority=priority, date=date)
+            obj.save()
+        return redirect('cbvtask')
 
 
-def update(request, updateid):
-    task = Task.objects.get(id=updateid)
-    form = Todoforms(request.POST or None, instance=task)
-    if form.is_valid():
-        form.save()
-        return redirect('/')
-    return render(request, 'edit.html', {'task': task, 'form': form})
+class TaskDetailView(DetailView):
+    model = Task
+    template_name = 'detail.html'
+    context_object_name = 'task'
 
 
-def delete(request, taskid):
-    task = Task.objects.get(id=taskid)
-    if request.method == "POST":
-        task.delete()
-        return redirect('/')
-    return render(request, 'delete.html', {'task': task})
+class TaskUpdateView(UpdateView):
+    model = Task
+    template_name = 'update.html'
+    context_object_name = 'task'
+    fields = ['name', 'priority', 'date']
+
+    def get_success_url(self):
+        return reverse_lazy('cbvdetail', kwargs={'pk': self.object.id})
+
+
+class TaskDeleteView(DeleteView):
+    model = Task
+    template_name = 'delete.html'
+    success_url = reverse_lazy('cbvtask')
